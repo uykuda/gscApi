9e0c8f61fe2a590f6f836d2fc0b5080a04193093
uyku <uykuda2@hotmail.com>
2022-11-18 16:56:27 +0300

    Some fixes and code adjustments

--- Api/Cs/discord.cs
+++ Api/Cs/discord.cs
@@ -11,47 +11,70 @@ namespace Api
         //For example, just before your main loop, on OnEnable for unity.
         public void Initialize()
         {
-            minecraft minecraft = new minecraft();
-            Program Program = new Program();
-            /*
-			Create a Discord client
-			NOTE: 	If you are using Unity3D, you must use the full constructor and define
-					 the pipe connection.
-			*/
-            client = new DiscordRpcClient("937388403517968475");
-
-            //Set the logger
-            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
-
-            //Subscribe to events
-            client.OnReady += (sender, e) =>
+            try
             {
-                Console.WriteLine("Hazır veren istemci: {0}", e.User.Username);
-            };
+                minecraft minecraft = new minecraft();
+                Program Program = new Program();
+                /*
+                Create a Discord client
+                NOTE: 	If you are using Unity3D, you must use the full constructor and define
+                         the pipe connection.
+                */
+                client = new DiscordRpcClient("937388403517968475");
 
-            client.OnPresenceUpdate += (sender, e) =>
-            {
-                Console.WriteLine("Istemci guncellendi! {0}", e.ApplicationID);
-            };
+                //Set the logger
+                client.Logger = new ConsoleLogger() { Level = LogLevel.None };
 
-            //Connect to the RPC
-            client.Initialize();
-            client.UpdateEndTime();
-            client.SetPresence(new RichPresence()
-            {
-
-                Details = "Creating Servers!",
-                State = "Now started " + Program.serverName,
-                Assets = new Assets()
+                //Subscribe to events
+                client.OnReady += (sender, e) =>
                 {
-                    LargeImageKey = "uyku",
-                    LargeImageText = "https://github.com/uykuda/gscApi",
+                    Console.WriteLine("Hazır veren istemci: {0}", e.User.Username);
+                };
+
+                client.OnPresenceUpdate += (sender, e) =>
+                {
+                    Console.WriteLine("Istemci guncellendi! {0}", e.ApplicationID);
+                };
+
+                //Connect to the RPC
+                client.Initialize();
+                client.UpdateEndTime();
+                client.SetPresence(new RichPresence()
+                {
+
+                    Details = "Creating Servers!",
+                    State = "Now started " + Program.serverName,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "uyku",
+                        LargeImageText = "https://github.com/uykuda/gscApi",
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                try
+                {
+                    client.Dispose();
                 }
-            });
+                catch (Exception d)
+                {
+                    Console.WriteLine(d);
+                }
+            }
         }
         public void Deinitialize()
         {
-            client.Dispose();
+
+            try
+            {
+                client.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
--- Api/Cs/minecraft.cs
+++ Api/Cs/minecraft.cs
@@ -50,57 +50,24 @@ namespace Api
             ini.Write("serverSoftware", Program.serverSoftware);
             ini.Write("serverVersion", Program.serverVersion);
             ini.Write("serverPort", Convert.ToString(Program.port));
-            if (Program.serverSoftware == "vanilla")
+            DownloadJar();
+        }
+        public void DownloadJar()
+        {
+            try
             {
-                try
+                using (var client = new WebClient())
                 {
-                    using (var client = new WebClient())
-                    {
-                        DirectoryInfo minecraft = Directory.CreateDirectory(Paths.minecraftServers + Program.serverName);
-                        client.DownloadFile(System.Configuration.ConfigurationManager.AppSettings.Get(Program.serverVersion), Paths.minecraftServers + Program.serverName + @"\" + Program.serverName + ".jar");
-                        Console.WriteLine(Program.serverVersion + " file downloaded successfully.");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
+                    DirectoryInfo minecraft = Directory.CreateDirectory(Paths.minecraftServers + Program.serverName);
+                    var value = System.Configuration.ConfigurationManager.AppSettings[Program.serverVersion + Program.serverSoftware[0]];
+                    client.DownloadFile(value, Paths.minecraftServers + Program.serverName + @"\" + Program.serverName + ".jar");
+                    Console.WriteLine(Program.serverVersion + " file downloaded successfully.");
                 }
             }
-            if (Program.serverSoftware == "spigot")
+            catch (Exception e)
             {
-                try
-                {
-                    using (var client = new WebClient())
-                    {
-                        DirectoryInfo minecraft = Directory.CreateDirectory(Paths.minecraftServers + Program.serverName);
-                        var value = System.Configuration.ConfigurationManager.AppSettings[Program.serverVersion + "s"];
-                        client.DownloadFile(value, Paths.minecraftServers + Program.serverName + @"\" + Program.serverName + ".jar");
-                        Console.WriteLine(Program.serverVersion + " file downloaded successfully.");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
+                Console.WriteLine(e);
             }
-            if (Program.serverSoftware == "purpur")
-            {
-                try
-                {
-                    using (var client = new WebClient())
-                    {
-                        DirectoryInfo minecraft = Directory.CreateDirectory(Paths.minecraftServers + Program.serverName);
-                        var value = System.Configuration.ConfigurationManager.AppSettings[Program.serverVersion + "p"];
-                        client.DownloadFile(value, Paths.minecraftServers + Program.serverName + @"\" + Program.serverName + ".jar");
-                        Console.WriteLine(Program.serverVersion + " file downloaded successfully.");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-
         }
         public void minecraftLoad()
         {
